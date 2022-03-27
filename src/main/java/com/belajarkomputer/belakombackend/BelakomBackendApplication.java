8977750a639a93911ce85eb06773223115cf3a1c
Jason Andersen Winfrey <jason.winfrey@gdn-commerce.com>
2022-03-27 22:29:54 +0700

    init

---
+++ src/main/java/com/belajarkomputer/belakombackend/BelakomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.belajarkomputer.belakombackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelakomBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BelakomBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/belajarkomputer/belakombackend/BelakomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belajarkomputer.belakombackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelakomBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
